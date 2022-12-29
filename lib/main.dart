b0c153aad49d3ff75fb814fd91aac19d985968a8
HP <darshitmendapara1425@gmail.com>
2022-12-29 19:14:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gstcalculator/demo.dart';
+
+import 'Home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GST(),
+    ),
+  );
+}
